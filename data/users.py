a0565a260c8aa5bfeb9bea631257468e73fd1fb7
Elena Ladyzhets <eladyzhets@gmail.com>
2025-03-03 21:35:36 -0600

    little names updated

---
+++ data/users.py
@@ -0,0 +1,36 @@
+import dataclasses
+from datetime import date
+
+
+@dataclasses.dataclass
+class User:
+    first_name: str
+    last_name:str
+    email:str
+    gender:str
+    phone_number:str
+    date_of_birth:date
+    subject1: str
+    subject2: str
+    hobby1: str
+    hobby2: str
+    hobby3: str
+    picture:str
+    address:str
+    state:str
+    city:str
+
+
+user = User(
+    first_name='Elena',
+    last_name='Ladyzhets',
+    email='eladyzhets@gmail.com',
+    gender='Female',
+    phone_number='9405979553',
+    date_of_birth=date(1997, 11, 15),
+    subject1='Physics', subject2='Maths',
+    hobby1='Sports', hobby2='Reading', hobby3='Music',
+    picture='1.png',
+    address='825 W.Sycamore st.',
+    state='NCR', city='Noida'
+)
--- test/test_the_form.py
+++ test/test_the_form.py
@@ -1,38 +1,9 @@
+from data import users
 from model.pages.registration_page import RegistrationPage
 
 def test_complete_and_submit_form():
     registration_page = RegistrationPage()
     registration_page.open()
-
-
-    registration_page.fill_first_name('Elena')
-    registration_page.fill_last_name('Ladyzhets')
-    registration_page.fill_email('eladyzhets@gmail.com')
-    registration_page.select_gender('Female')
-    registration_page.fill_phone_number('9405979553')
-    registration_page.fill_date_of_birth(year='1997', month='November', day='05')
-    registration_page.select_subject('Physics')
-    registration_page.select_subject('Maths')
-    registration_page.select_hobby('Sports')
-    registration_page.select_hobby('Reading')
-    registration_page.select_hobby('Music')
-    registration_page.upload_picture('1.jpg')
-    registration_page.fill_address('825 W.Sycamore st.')
-    registration_page.select_state('NCR')
-    registration_page.select_city('Noida')
-    registration_page.submit()
-
-
-    registration_page.should_registered_user_with(
-        full_name='Elena Ladyzhets',
-        email='eladyzhets@gmail.com',
-        gender='Female',
-        phone='9405979553',
-        date_of_birth='05 November,1997',
-        subjects='Physics, Maths',
-        hobbies='Sports, Reading, Music',
-        file_name='1.jpg',
-        address='825 W.Sycamore st.',
-        state='NCR',
-        city='Noida')
+    registration_page.register(users.user)
+    registration_page.should_registered_user_with(users.user)
 
